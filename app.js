83ac5cacbc18e562d863c190a3e59360307b91cd
idan535 <idanu55@gmail.com>
2024-10-24 03:58:24 +0300

    Add GitHub Actions workflow

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello, DevOps!!!!');
+  res.send('Hello, DevOps!!!!!!');
 });
 
 app.listen(PORT, () => {
